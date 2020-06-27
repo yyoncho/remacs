252b6c5c22c932cfcdb82d9840d862568269f0b4
Ivan Yonchovski <yyoncho@gmail.com>
2020-06-27 21:14:03 +0300

    POC of receiving messages and polling it from emacs main thread

--- rust_src/src/editfns.rs
+++ rust_src/src/editfns.rs
@@ -1695,4 +1695,112 @@ pub fn system_name() -> LispStringRef {
     unsafe { Fsystem_name() }.into()
 }
 
+use crate::remacs_sys::make_user_ptr;
+use crate::remacs_sys::Lisp_User_Ptr;
+use crossbeam_channel::{bounded, Receiver, Sender};
+use lsp_server::Message;
+use std::{os::raw::c_void, thread};
+
+/// Creates an LSP connection via stdio.
+fn stdio_client() -> JsonRpcStdio {
+    let process: process::Child = Command::new("/bin/cat")
+        .arg("/home/kyoncho/file.txt")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed to spawn child process");
+
+    let (writer, writer_receiver) = bounded::<Message>(0);
+
+    let mut inn = process.stdin;
+    let writer_thread = thread::spawn(move || {
+        let mut stdout_writer = BufWriter::new(inn.as_mut().unwrap());
+        writer_receiver
+            .into_iter()
+            .try_for_each(|it| it.write(&mut stdout_writer))?;
+        Ok(())
+    });
+
+    let (reader_sender, reader) = bounded::<Message>(0);
+    let mut out = process.stdout;
+    let reader_thread = thread::spawn(move || {
+        let mut stdout_reader = BufReader::new(out.as_mut().unwrap());
+        while let Some(msg) = Message::read(&mut stdout_reader)? {
+            println!("[bkg thread] >>> {:?}", msg);
+            match reader_sender.send(msg) {
+                Ok(a) => a,
+                Err(msg1) => unsafe {
+                    println!("error when sending >>> {:?}", msg1);
+                },
+            };
+        }
+        println!("XXX >>> exiting...");
+        Ok(())
+    });
+
+    JsonRpcStdio {
+        reader,
+        writer,
+        reader_thread,
+        writer_thread,
+    }
+}
+
+struct JsonRpcStdio {
+    reader: Receiver<Message>,
+    writer: Sender<Message>,
+    reader_thread: thread::JoinHandle<io::Result<()>>,
+    writer_thread: thread::JoinHandle<io::Result<()>>,
+}
+
+impl JsonRpcStdio {
+    fn finalize() {}
+}
+
+thread_local! {
+    static STORAGE: RefCell<Option<JsonRpcStdio>> = RefCell::new(None);
+}
+
+use std::{
+    cell::RefCell,
+    io::{self, BufReader, BufWriter},
+    process::{self, Command, Stdio},
+};
+
+#[lisp_fn(min = "1")]
+pub fn fastjsonrcp_connection(_input: LispObject) -> LispObject {
+    let connection = Box::into_raw(Box::new(stdio_client())) as *mut c_void;
+    unsafe { make_user_ptr(Some(finalize), connection) }
+}
+
+#[no_mangle]
+pub extern "C" fn finalize(_data: *mut c_void) {}
+
+#[lisp_fn(min = "1")]
+pub fn fastjsonrcp_get_message(input: LispObject) -> LispObject {
+    let connection = unsafe {
+        let up = input.get_untaggedptr() as *mut Lisp_User_Ptr;
+        (*up).p as *mut _ as *mut JsonRpcStdio
+    };
+
+    unsafe {
+        match (*connection).reader.try_recv() {
+            Ok(a) => {
+                println!("[main-thread rust] received from bkg thread: {:?}", a);
+                // input
+            }
+
+            Err(msg) => {
+                println!("[main-thread rust] received from bkg thread: {:?}", msg);
+                unsafe {
+                    message1(format!("{}", msg).as_ptr() as *const ::libc::c_char);
+                }
+                // input
+            }
+        }
+    }
+
+    input
+}
+
 include!(concat!(env!("OUT_DIR"), "/editfns_exports.rs"));
